03b703f57d65b14320ee3290c66e2c839f173555
Jonathan Taylor <jonathan.taylor@stanford.edu>
2021-06-15 16:36:13 -0700

    adding smpoly and smarize for Ch3

--- ISLP/__init__.py
+++ ISLP/__init__.py
@@ -1,3 +1,4 @@
+
 from os.path import join as pjoin
 import pandas as pd, numpy as np
 from pkg_resources import resource_filename
@@ -32,3 +33,40 @@ def load_data(dataset):
     else:
         filename = resource_filename('ISLP', pjoin('data', '%s.csv' % dataset))
         return pd.read_csv(filename)
+
+def smarize(results):
+    """
+    Take a fit statsmodels and summarize it
+    by returning the usual coefficient estimates,
+    their standard errors, the usual test
+    statistics and P-values as well as 95%
+    confidence intervals
+
+    Based on:
+
+    https://stackoverflow.com/questions/51734180/converting-statsmodels-summary-object-to-pandas-dataframe
+    """
+    tab = results.summary().tables[1]
+    return pd.read_html(tab.as_html(),
+                        index_col=0,
+                        header=0)[0]
+
+def smpoly(X, degree):
+    """  
+    Create columns of design matrix
+    for orthogonal polynomial for a given series X
+    """
+
+    result = np.zeros((X.shape[0], degree))
+    powX = np.power.outer(X.values, np.arange(1, degree+1))
+    powX -= powX.mean(0)
+    result[:,0] = powX[:,0] / np.linalg.norm(powX[:,0])
+
+    for i in range(1, degree):
+        result[:,i] = powX[:,i]
+        for j in range(i):
+            result[:,i] -= (result[:,i] * result[:,j]).sum() * result[:,j]
+        result[:,i] /= np.linalg.norm(result[:,i])
+    df = pd.DataFrame(result, columns=['poly(%s, %d)' % (X.name, degree) 
+                                       for degree in range(1, degree+1)])
+    return df
