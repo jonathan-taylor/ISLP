47bb1bff837e7a35260aa29d968ea307c192fb2c
Jonathan Taylor <jonathan.taylor@stanford.edu>
2023-01-13 00:03:48 -0800

    docstring

--- ISLP/__init__.py
+++ ISLP/__init__.py
@@ -1,3 +1,9 @@
+"""
+**ISLP**  is a Python library to accompany
+*Introduction to Statistical Learning with applications in Python*.
+See the `statistical learning homepage <http://statlearning.com/>`_
+for more details.
+"""
 
 from os.path import join as pjoin
 import pandas as pd, numpy as np
