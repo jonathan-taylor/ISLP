5d085d301c7e58ecc78dd42dc878b773f9bc5b73
Jonathan Taylor <jonathan.taylor@stanford.edu>
2023-07-31 01:21:32 -0400

    using context manager

--- ISLP/__init__.py
+++ ISLP/__init__.py
@@ -14,21 +14,26 @@ from importlib.resources import (as_file,
 
 def load_data(dataset):
     if dataset == 'NCI60':
-        features = as_file(files('ISLP').joinpath('data', 'NCI60data.npy'))
-        X = np.load(features)
-        labels = as_file(files('ISLP').joinpath('data', 'NCI60labs.csv'))
-        Y = pd.read_csv(labels)
+        with as_file(files('ISLP').joinpath('data', 'NCI60data.npy')) as features:
+            X = np.load(features)
+            labels = as_file(files('ISLP').joinpath('data', 'NCI60labs.csv'))
+            Y = pd.read_csv(labels)
         return {'data':X, 'labels':Y}
     elif dataset == 'Khan':
-        xtest = pd.read_csv(as_file(files('ISLP').joinpath('data', 'Khan_xtest.csv')))
+        with as_file(files('ISLP').joinpath('data', 'Khan_xtest.csv')) as xtest:
+            xtest = pd.read_csv(xtest)
         xtest = xtest.rename(columns=dict([('V%d' % d, 'G%04d' % d) for d in range(1, len(xtest.columns)+0)]))
-        ytest = pd.read_csv(as_file(files('ISLP').joinpath('data', 'Khan_ytest.csv')))
+        with as_file(files('ISLP').joinpath('data', 'Khan_ytest.csv')) as ytest:
+            ytest = pd.read_csv(ytest)
         ytest = ytest.rename(columns={'x':'Y'})
         ytest = ytest['Y']
         
-        xtrain = pd.read_csv(as_file(files('ISLP').joinpath('data', 'Khan_xtrain.csv')))
-        xtrain = xtrain.rename(columns=dict([('V%d' % d, 'G%04d' % d) for d in range(1, len(xtest.columns)+0)]))
-        ytrain = pd.read_csv(as_file(files('ISLP').joinpath('data', 'Khan_ytrain.csv')))
+        with as_file(files('ISLP').joinpath('data', 'Khan_xtrain.csv')) as xtrain:
+            xtrain = pd.read_csv(xtrain)
+            xtrain = xtrain.rename(columns=dict([('V%d' % d, 'G%04d' % d) for d in range(1, len(xtest.columns)+0)]))
+
+        with as_file(files('ISLP').joinpath('data', 'Khan_ytrain.csv')) as ytrain:
+        ytrain = pd.read_csv(ytrain)
         ytrain = ytrain.rename(columns={'x':'Y'})
         ytrain = ytrain['Y']
 
@@ -36,35 +41,38 @@ def load_data(dataset):
                 'xtrain':xtrain,
                 'ytest':ytest,
                 'ytrain':ytrain}
+
     elif dataset == 'Hitters':
-        filename = as_file(files('ISLP').joinpath('data', '%s.csv' % dataset))
-        df = pd.read_csv(filename)
+        with as_file(files('ISLP').joinpath('data', '%s.csv' % dataset)) as filename:
+            df = pd.read_csv(filename)
         for col in ['League', 'Division', 'NewLeague']:
             df[col] = pd.Categorical(df[col])
         return df
     elif dataset == 'Carseats':
-        filename = as_file(files('ISLP').joinpath('data', '%s.csv' % dataset))
-        df = pd.read_csv(filename)
+        with as_file(files('ISLP').joinpath('data', '%s.csv' % dataset)) as filename:
+            df = pd.read_csv(filename)
         for col in ['ShelveLoc', 'Urban', 'US']:
             df[col] = pd.Categorical(df[col])
         return df
     elif dataset == 'NYSE':
-        filename = as_file(files('ISLP').joinpath('data', '%s.csv' % dataset))
-        df = pd.read_csv(filename).set_index('date')
+        with as_file(files('ISLP').joinpath('data', '%s.csv' % dataset)) as filename: 
+            df = pd.read_csv(filename).set_index('date')
         return df
     elif dataset == 'Publication':
-        df = pd.read_csv(as_file(files('ISLP').joinpath('data', 'Publication.csv')))
+        with as_file(files('ISLP').joinpath('data', 'Publication.csv')) as f:
+            df = pd.read_csv(f)
         for col in ['mech']:
             df[col] = pd.Categorical(df[col])
         return df
     elif dataset == 'BrainCancer':
-        df = pd.read_csv(as_file(files('ISLP').joinpath('data', 'BrainCancer.csv')))
+        with as_file(files('ISLP').joinpath('data', 'BrainCancer.csv')) as f:
+            df = pd.read_csv(f)
         for col in ['sex', 'diagnosis', 'loc', 'stereo']:
             df[col] = pd.Categorical(df[col])
         return df
     elif dataset == 'Bikeshare':
-        filename = as_file(files('ISLP').joinpath('data', '%s.csv' % dataset))
-        df = pd.read_csv(filename)
+        with as_file(files('ISLP').joinpath('data', '%s.csv' % dataset)) as filename:
+            df = pd.read_csv(filename)
         df['weathersit'] = pd.Categorical(df['weathersit'], ordered=False)
         # setting order to avoid alphabetical
         df['mnth'] = pd.Categorical(df['mnth'],
@@ -80,12 +88,13 @@ def load_data(dataset):
                                   categories=range(24))
         return df
     elif dataset == 'Wage':
-        df = pd.read_csv(as_file(files('ISLP').joinpath('data', 'Wage.csv')))
-        df['education'] = pd.Categorical(df['education'], ordered=True)
+        with as_file(files('ISLP').joinpath('data', 'Wage.csv')) as f:
+            df = pd.read_csv(f)
+            df['education'] = pd.Categorical(df['education'], ordered=True)
         return df
     else:
-        filename = as_file(files('ISLP').joinpath('data', '%s.csv' % dataset))
-        return pd.read_csv(filename)
+        with as_file(files('ISLP').joinpath('data', '%s.csv' % dataset)) as filename:
+            return pd.read_csv(filename)
 
 from sklearn.metrics import confusion_matrix as _confusion_matrix
 
